2f5cb638a7d86a66e8e2eba837711ecd0d269376
Lubber Driver <lubberdriver@gmail.com>
2025-07-29 16:01:23 -0300

    Corrige rota principal para permitir subrotas como /jogar/:token

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import JogarToken from "./pages/JogarToken";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/jogar/:token" element={<JogarToken />} />
+        <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
